30d87a23b1f80419cdeba71e52bd0daa7b519f03
Inna 1201 <inna.ogurcova@gmail.com>
2022-09-25 21:58:56 +0100

    created query.sql, tested tabs, working on index.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,26 @@
-INSERT INTO department (id, name)
-VALUES (1, "Sales"),
-       (2, "Engineering"),
-       (3, "Finance"),
-       (4, "Legal");
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (1, "Sales Lead", 1000000, 1),
-       (2, "Salesperson", 80000, 1),
-       (3, "Lead Engineer", 150000, 2),
-       (4, "Software Engineer", 120000, 2),
-       (5, "Account Manager", 160000, 3),
-       (6, "Accountant", 125000, 3),
-       (7, "Legal Team Lead", 250000, 4),
-       (8, "Lawyer", 190000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 1000000, 1),
+       ("Salesperson", 80000, 1),
+       ("Lead Engineer", 150000, 2),
+       ("Software Engineer", 120000, 2),
+       ("Account Manager", 160000, 3),
+       ("Accountant", 125000, 3),
+       ("Legal Team Lead", 250000, 4),
+       ("Lawyer", 190000, 4);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Kirsty" "Morton", "Sales Lead", 1),
-       (2, "Carl" "Patel", "Salesperson", 2, 1),
-       (3, "Phillip" "Potter", "Lead Engineer", 3),
-       (4, "Heather" "Owens", "Software Engineer", 4, 3),
-       (5, "Martine" "Chan", "Account Manager", 5),
-       (6, "Richard" "Watson", "Accountant", 6, 5),
-       (7, "Helen" "Mills", "Legal Team Lead", 7),
-       (8, "Karen" "Skene", "Lawyer", 8, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kirsty" "Morton", "Sales Lead", 1, null),
+       ("Carl" "Patel", "Salesperson", 2, 1),
+       ("Phillip" "Potter", "Lead Engineer", 3, null),
+       ("Heather" "Owens", "Software Engineer", 4, 3),
+       ("Martine" "Chan", "Account Manager", 5, null),
+       ("Richard" "Watson", "Accountant", 6, 5),
+       ("Helen" "Mills", "Legal Team Lead", 7, null),
+       ("Karen" "Skene", "Lawyer", 8, 7);
 
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const inquirer = require('inquirer');
+// const db = require('./db/schema.sql')
 
 
 function mainMenu() {
     inquirer.prompt({
-        type: 'input',
+        type: 'list',
         message: 'What would you like todo?',
         name: 'main',
         choices: [
@@ -14,15 +15,60 @@ function mainMenu() {
             'Add Role',
             'View All Departments',
             'Add Department',
-            'Quit',
-            'View all Employees'
+            'Quit'
         ],
-        default: '(Move up and down to reveal more choices.)'
+        default: 'Move up and down to reveal more choices.'
 
     }).then(answers => {
+        if (answers.type === "View All Employees") {
+            // show emplyee+role+department tables
+            viewEmployees()
+
+        }
+
+        if (answers.type === "Add Emplyee") {
+            addEmployee()
+        }
+
+        if (answers.type === "Update Employee Role") {
+            updateRole()
+        }
+
+        if (answers.type === "View All Roles") {
+            // show role+department table
+        }
+
+        if (answers.type === "Add Role") {
+            addRole()
+        }
+
+        if (answers.type === "View All Departments") {
+            // show department table
+        }
+
+        if (answers.type === "Add Department") {
+            addDepartment()
+        }
+
+        if (answers.type === "Quit") {
+            // exit app???
+        }
 
     })
 };
+
+ const viewEmployees = () => {
+                db.query(`SELECT 
+                employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, employee.manager_id
+                FROM employee
+                JOIN role ON employee.role_id = role.id
+                JOIN department ON department.id = role.department_id;`
+                ), (err, result) => {
+                    console.table(result)
+                }
+                mainMenu() 
+            }
+
 
 function addEmployee() {
     inquirer.prompt(
@@ -38,7 +84,7 @@ function addEmployee() {
                 name: 'lastName',
             },
             {
-                type: 'input',
+                type: 'list',
                 message: 'Please use arrow keys to choose the role.',
                 name: 'role',
                 choices: [
@@ -54,9 +100,9 @@ function addEmployee() {
                 default: '(Move up and down to reveal more choices.)'
             },
             {
-                type: 'input',
+                type: 'list',
                 message: 'Please use arrow keys to choose the manager.',
-                name: 'role',
+                name: 'manager',
                 choices: [
                     'Kirsty Morton',
                     'Phillip Potter',
@@ -68,15 +114,20 @@ function addEmployee() {
 
 
         ]).then(answers => {
+            const employee = (answers.firstName, answers.lastName, answers.role, answers.manager)
+            console.log(employee)
+            // add records to table
+            mainMenu()
 
         })
 };
+
 
 function updateRole() {
     inquirer.prompt(
         [
             {
-                type: 'input',
+                type: 'list',
                 message: 'Please select employee whch role you want to update.',
                 name: 'selectEmployee',
                 choices: [
@@ -93,7 +144,7 @@ function updateRole() {
 
             },
             {
-                type: 'input',
+                type: 'list',
                 message: 'Please choose the role that you want to assign the selected employee',
                 name: 'selectRole',
                 choices: [
@@ -104,12 +155,14 @@ function updateRole() {
                     'Accountant',
                     'Legal Team Lead',
                     'Lawyer',
-                    'Salesperson' // how to add new rolet to this list
+                    'Salesperson' // how to add new roles to this list
                 ],
                 default: '(Move up and down to reveal more choices.)'
             }
 
         ]).then(answers => {
+             // add records to table
+             mainMenu()
 
         })
 };
@@ -129,7 +182,7 @@ function addRole() {
                 name: 'roleSalary',
             },
             {
-                type: 'input',
+                type: 'list',
                 message: 'Which department does the role belongs to?',
                 name: 'role',
                 choices: [
@@ -141,6 +194,8 @@ function addRole() {
             },
 
         ]).then(answers => {
+             // add records to table
+             mainMenu()
 
         })
 };
@@ -153,6 +208,14 @@ function addDepartment() {
                 name: 'departmentName'
         
             }).then(answers => {
+                 // add records to table
+            mainMenu()
 
         })
 };
+
+mainMenu();
+// addEmployee();
+// updateRole();
+// addRole();
+// addDepartment();
